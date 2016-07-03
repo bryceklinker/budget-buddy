ade875271d30f347bf128f50dd785277ef4a393b
Bryce Klinker <bryce.klinker@gmail.com>
2016-07-03 01:54:34 -0500

    Changing location for test run.

--- build/ci-build.ps1
+++ build/ci-build.ps1
@@ -4,16 +4,18 @@ $testDirectories = Get-ChildItem -Path ".\test" | ? { $_.PSIsContainer } | Selec
 Write-Host $testDirectories;
 
 $hasError = $false;
+
+Set-Location "src/Api";
 foreach ($testDirectory in $testDirectories) {
 	dotnet test $testDirectory.FullName
 	if ($LASTEXITCODE -ne 0) {
 		$hasError = $true;
 	}
 }
+Set-Location "";
 
 if ($hasError) {
-	exit;
+	exit 1;
 }
 
-$apiDirector = ".\"
 dotnet publish ".\src\Api\project.json";
