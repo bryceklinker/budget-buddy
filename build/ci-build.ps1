043d7289e4cb93fff7efd96569da3cd2ce27179e
Bryce Klinker <bryce.klinker@gmail.com>
2016-07-08 01:35:53 -0500

    Fixing ci script.

--- build/ci-build.ps1
+++ build/ci-build.ps1
@@ -1,31 +1,57 @@
-dotnet restore;
-
-$testDirectories = Get-ChildItem -Path ".\test" | ? { $_.PSIsContainer } | Select-Object FullName;
-Write-Host $testDirectories;
-
-$hasError = $false;
-
-pushd "src/Api"
-foreach ($testDirectory in $testDirectories) {
-	dotnet test $testDirectory.FullName
+function Exit-On-Error
+{
 	if ($LASTEXITCODE -ne 0) {
-		$hasError = $true;
+		exit $LASTEXITCODE;
 	}
 }
-popd
 
-pushd "src/Web"
+function Build-Api 
+{
+	dotnet restore;
 
-npm install;
+	$testDirectories = Get-ChildItem -Path ".\test" | ? { $_.PSIsContainer } | Select-Object FullName;
+	Write-Host $testDirectories;
 
-npm run test:single;
-
-npm run build;
-
-popd
-
-if ($hasError) {
-	exit 1;
+	pushd "src/Api"
+	foreach ($testDirectory in $testDirectories) {
+		dotnet test $testDirectory.FullName
+		Exit-On-Error;
+	}
+	popd
 }
 
-dotnet publish src\Api\project.json -c Release;
+function Build-Client
+{
+	pushd "src/Web";
+
+	npm install;
+	Exit-On-Error;
+
+	npm run test:single;
+	Exit-On-Error;
+
+	npm run build;
+	Exit-On-Error;
+
+	popd;
+}
+
+function Package-App 
+{
+	if (Test-Path "./src/Api/wwwroot") {
+		Remove-Item -Path "./src/Api/wwwroot" -Force -Recurse;
+	}
+	
+	Copy-Item -Path "./src/Web/dist" -Destination "./src/Api/wwwroot" -Recurse;
+	dotnet publish src\Api\project.json -c Release;
+
+	Remove-Item -Path "./src/Api/wwwroot" -Force -Recurse;
+}
+
+Build-Api;
+Exit-On-Error;
+
+Build-Client;
+Exit-On-Error;
+
+Package-App;
