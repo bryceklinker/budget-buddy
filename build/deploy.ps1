ac59fbfa2484d53c44480e86541ef32c494b065c
Bryce Klinker <bryce.klinker@gmail.com>
2016-07-17 22:08:30 -0500

    Added logging to deploy script

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -1,20 +1,24 @@
 $budgetBuddyDir = [environment]::GetEnvironmentVariable('budget-buddy-dir');
 $budgetBuddyServiceName = 'Budget-Buddy'
 
-function Copy-Client
+function Copy-Api
 {
-	pushd "./src/Api/bin/Release/net461"
+	pushd ".\src\Api\bin\Release\net461"
 
-	Copy-Item "./*" $budgetBuddyDir
+	Write-Host "Coping Aai files..."
+	Copy-Item ".\*" $budgetBuddyDir
+	Write-Host "Finished copying api files."
 
 	popd
 }
 
-function Copy-Api 
+function Copy-Client 
 {
-	pushd "./src/Web/dist"
+	pushd ".\src\Web\dist"
 	
-	Copy-Item "./*" "$budgetBuddyDir/wwwroot"
+	Write-Host "Copying client files..."
+	Copy-Item ".\*" "$budgetBuddyDir/wwwroot"
+	Write-Host "Finished copying client files."
 		
 	popd
 }
@@ -22,7 +26,9 @@ function Copy-Api
 function Ensure-Service-Exists 
 {
 	if(Get-Service -Name $budgetBuddyServiceName -ErrorAction SilentlyContinue) {
+		Write-Host "Creating $budgetBuddyServiceName..."
 		New-Service -Name $budgetBuddyServiceName -BinaryPathName "$budgetBuddyDir\api.exe" -StartupType Automatic
+		Write-Host "Finished creating $budgetBuddyServiceName."
 	}
 }
 
@@ -30,14 +36,18 @@ function Stop-Service
 {
 	Ensure-Service-Exists
 
+	Write-Host "Stopping service..."
 	Stop-Service $budgetBuddyServiceName
+	Write-Host "Finished stopping service."
 }
 
 function Start-Service
 {
 	Ensure-Service-Exists
 
+	Write-Host "Starting service..."
 	Start-Service $budgetBuddyServiceName
+	Write-Host "Finished starting service."
 }
 
 Stop-Service
