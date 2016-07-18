5ddd8c8b22c79f00cb36557c1cc2da4e763b2cf6
Bryce Klinker <bryce.klinker@gmail.com>
2016-07-17 22:44:33 -0500

    Fixing run as service configuration

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using BudgetBuddy.Api.Bootstrap;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.WindowsServices;
@@ -10,18 +11,35 @@ namespace BudgetBuddy.Api
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
+            var builder = new WebHostBuilder()
                 .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
+
+            if (IsDebugging())
+            {
+                builder = builder.UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseIISIntegration();
+            }
+            else
+            {
+                var exePath = Process.GetCurrentProcess().MainModule.FileName;
+                var directory = Path.GetDirectoryName(exePath);
+                builder = builder.UseContentRoot(directory)
+                    .UseUrls("http://+:8000");
+            }
+
+            var host = builder.Build();
 
             var hostingEnvironment = host.Services.GetService<IHostingEnvironment>();
             if (hostingEnvironment.IsProduction())
                 host.RunAsService();
             else
                 host.Run();
+        }
+
+        private static bool IsDebugging()
+        {
+            return Debugger.IsAttached;
         }
     }
 }
